1b9dfa7a732d3d2a68ea4c14ae5b09765bd4804f
Lakshman <57407426+lakshmanboddoju@users.noreply.github.com>
2019-11-11 13:16:35 -0600

    Update and rename hello.bat to 22-hello.bat

---
+++ 9/22-hello.bat
@@ -0,0 +1,2 @@
+@py D:\Users\lakshman\Documents\GitHub-lakshmanboddoju\Automate_the_boring_stuff_with_python\8\22-hello.py %*
+@pause
--- 9/hello.bat
+++
@@ -1,2 +0,0 @@
-@py D:\Users\lakshman\Documents\GitHub-lakshmanboddoju\Automate_the_boring_stuff_with_python\8\hello.py %*
-@pause
