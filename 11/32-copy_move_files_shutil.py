5627a3e0d8bb00755cdefb93edce7356688aaa30
Lakshman <57407426+lakshmanboddoju@users.noreply.github.com>
2019-11-12 16:46:03 -0600

    Update 32-copy_move_files_shutil.py

--- 11/32-copy_move_files_shutil.py
+++ 11/32-copy_move_files_shutil.py
@@ -4,9 +4,9 @@ import shutil
 
 # simple usage example
 
-shutil.copy('C:\\Users\\pavan\\Desktop\\hello3.txt', 'D:\\Users\\pavan\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11\\hello3copy.txt')
+shutil.copy('C:\\Users\\lakshman\\Desktop\\hello3.txt', 'D:\\Users\\lakshman\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11\\hello3copy.txt')
 
-shutil.copytree('D:\\Users\\pavan\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11', 'D:\\Users\\pavan\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11_backup')
+shutil.copytree('D:\\Users\\lakshman\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11', 'D:\\Users\\lakshman\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11_backup')
 
-shutil.move('D:\\Users\\pavan\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11\\hello3copy.txt','D:\\Users\\pavan\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python')
+shutil.move('D:\\Users\\lakshman\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11\\hello3copy.txt','D:\\Users\\lakshman\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python')
 
