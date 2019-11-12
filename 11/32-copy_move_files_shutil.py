65a4f9e7591459d3e36433756a2bf9a602d98bdd
Lakshman <57407426+lakshmanboddoju@users.noreply.github.com>
2019-11-12 16:45:18 -0600

    Add files via upload

---
+++ 11/32-copy_move_files_shutil.py
@@ -0,0 +1,12 @@
+#! python3
+
+import shutil
+
+# simple usage example
+
+shutil.copy('C:\\Users\\pavan\\Desktop\\hello3.txt', 'D:\\Users\\pavan\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11\\hello3copy.txt')
+
+shutil.copytree('D:\\Users\\pavan\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11', 'D:\\Users\\pavan\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11_backup')
+
+shutil.move('D:\\Users\\pavan\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python\\11\\hello3copy.txt','D:\\Users\\pavan\\Documents\\GitHub-lakshmanboddoju\\Automate_the_boring_stuff_with_python')
+
