e8428ebed1f81f82d23d1feb4660c4ad010fc3b9
Lakshman <57407426+lakshmanboddoju@users.noreply.github.com>
2019-11-12 15:48:16 -0600

    Add files via upload

---
+++ 11/31-hello_txt_read.py
@@ -0,0 +1,37 @@
+#! python3
+
+import shelve
+
+# Read Operation
+
+helloFile = open('C:\\Users\\pavan\\Desktop\\hello.txt')
+#helloFileContent = helloFile.read()
+#print(helloFileContent)
+print(helloFile.readlines())
+helloFile.close()
+
+
+# Write Operation
+
+helloFile2 = open('C:\\Users\\pavan\\Desktop\\hello2.txt', 'w')
+helloFile2.write('YOLO!\n')
+helloFile2.write('You Rock!\n')
+helloFile2.close()
+
+
+# Append Operation
+
+helloFile3 = open('C:\\Users\\pavan\\Desktop\\hello3.txt', 'a')
+helloFile3.write('YOLO!\n')
+helloFile3.write('You Rock!\n')
+helloFile3.close()
+
+
+# Shelve File | Persistent memory for complex stuff to store like lists, etc.
+
+shelfFile = shelve.open('mydata')
+shelfFile['cats'] = ['Zophie', 'Pooka', 'Simon', 'Fat-tail', 'Cleo']
+shelfFile.close()
+
+shelfFile = shelve.open('mydata')
+print(shelfFile['cats'])
