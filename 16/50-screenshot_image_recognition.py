7702b1697ceb7aa6223137a18e81d6bc6c7fb979
Lakshman <57407426+lakshmanboddoju@users.noreply.github.com>
2019-11-18 12:13:42 -0600

    Update 50-screenshot_image_recognition.py

--- 16/50-screenshot_image_recognition.py
+++ 16/50-screenshot_image_recognition.py
@@ -2,9 +2,9 @@
 
 import pyautogui
 
-pyautogui.screenshot(r'D:\Users\pavan\Documents\GitHub-lakshmanboddoju\Automate_the_boring_stuff_with_python\16\screenshot_example.png')
+pyautogui.screenshot(r'D:\Automate_the_boring_stuff_with_python\16\screenshot_example.png')
 
-print(pyautogui.locateOnScreen(r'D:\Users\pavan\Documents\GitHub-lakshmanboddoju\Automate_the_boring_stuff_with_python\16\example.png'))
+print(pyautogui.locateOnScreen(r'D:\Automate_the_boring_stuff_with_python\16\example.png'))
 
-pyautogui.moveTo((pyautogui.locateCenterOnScreen(r'D:\Users\pavan\Documents\GitHub-lakshmanboddoju\Automate_the_boring_stuff_with_python\16\example.png')), duration=1)
+pyautogui.moveTo((pyautogui.locateCenterOnScreen(r'D:\Automate_the_boring_stuff_with_python\16\example.png')), duration=1)
 
